63add9d6a44c7dcd73b2143ce6805d08b7851f88
Surendhar-Vkl <surendhar2207@gmail.com>
2023-08-21 08:55:13 +0530

    added login form with inputboxes and created a form validation(Validator logic missing)

--- lib/main.dart
+++ lib/main.dart
@@ -28,18 +28,66 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  final _LoginPage = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text(
+          title: const Text(
             "Pagadai",
             style: TextStyle(fontSize: 24),
           ),
           backgroundColor: Colors.indigo,
         ),
-        body: Center(
-          child: Text("Hello World"),
-        ));
+        body: Column(children: <Widget>[
+          Text("Hello World"),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter a Username',
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+            child: TextFormField(
+              key: _LoginPage,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your Password',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter some text';
+                }
+                return 'please enter';
+              },
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 16),
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Validate returns true if the form is valid, or false otherwise.
+                    print('Button Pressed');
+                    if (_LoginPage.currentState!.validate()) {
+                      // If the form is valid, display a snackbar. In the real world,
+                      // you'd often call a server or save the information in a database.
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('Processing Data')),
+                      );
+                    }
+                  },
+                  child: const Text('Submit'),
+                ),
+              ),
+            ],
+          ),
+        ]));
   }
 }
